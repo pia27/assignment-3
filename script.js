a3d236f579b6000dcc0bf8e08dbfe0363e106520
pia27 <apalamitra20@gmail.com>
2020-09-29 00:14:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,119 @@
+var arrHead = new Array();	// array for header.
+arrHead = [ ' '];
+
+// first create TABLE structure with the headers. 
+function createTable() {
+    var empTable = document.createElement('table');
+    empTable.setAttribute('id', 'empTable'); // table id.
+    var empTable = document.createElement('table');
+    empTable.setAttribute('id', 'empTable'); // table id.
+
+    var tr = empTable.insertRow(-1);
+    for (var h = 0; h < arrHead.length; h++) {
+        var th = document.createElement('th'); // create table headers
+        th.innerHTML = arrHead[h];
+        tr.appendChild(th);
+    }
+
+    var div = document.getElementById('cont');
+    div.appendChild(empTable);  // add the TABLE to the container.
+}
+
+// now, add a new to the TABLE.
+function addRow() {
+    var empTab = document.getElementById('empTable');
+
+    var rowCnt = empTab.rows.length;   // table row count.
+    var tr = empTab.insertRow(rowCnt); // the table row.
+    tr = empTab.insertRow(rowCnt);
+
+    for (var c = 0; c < arrHead.length; c++) {
+        var td = document.createElement('td'); // table definition.
+        td = tr.insertCell(c);
+        {
+        if(c == 0){
+            
+                // 2nd column, will have textbox.
+                var ele = document.createElement('input');
+                ele.setAttribute('type', 'file');
+                ele.setAttribute('value', '');
+    
+                td.appendChild(ele);
+    
+        }
+
+          {      // the first column.
+            // add a button in every new row in the first column.
+            var button = document.createElement('input');
+
+            // set input attributes.
+            button.setAttribute('type', 'button');
+            button.setAttribute('value', 'Delete');
+
+            // add button's 'onclick' event.
+            button.setAttribute('onclick', 'removeRow(this)');
+
+            td.appendChild(button);
+        }
+    }
+    }
+}
+
+// delete TABLE row function.
+function removeRow(oButton) {
+    var empTab = document.getElementById('empTable');
+    empTab.deleteRow(oButton.parentNode.parentNode.rowIndex); // button -> td -> tr.
+}
+
+// function to extract and submit table data.
+function submit() {
+    var myTab = document.getElementById('empTable');
+    var arrValues = new Array();
+
+    // loop through each row of the table.
+    for (row = 1; row < myTab.rows.length - 1; row++) {
+      // loop through each cell in a row.
+        for (c = 0; c < myTab.rows[row].cells.length; c++) {  
+            var element = myTab.rows.item(row).cells[c];
+            if (element.childNodes[0].getAttribute('type') == 'text') {
+                arrValues.push("'" + element.childNodes[0].value + "'");
+            }
+        }
+    }
+    
+    // The final output.
+    document.getElementById('output').innerHTML = arrValues;
+    //console.log (arrValues);   
+}
+
+
+
+// delete TABLE row function.
+function removeRow(oButton) {
+    var empTab = document.getElementById('empTable');
+    empTab.deleteRow(oButton.parentNode.parentNode.rowIndex); // button -> td -> tr.
+}
+
+// function to extract and submit table data.
+function submit() {
+    var myTab = document.getElementById('empTable');
+    var arrValues = new Array();
+
+    // loop through each row of the table.
+    for (row = 1; row < myTab.rows.length - 1; row++) {
+      // loop through each cell in a row.
+        for (c = 0; c < myTab.rows[row].cells.length; c++) {  
+            var element = myTab.rows.item(row).cells[c];
+            if (element.childNodes[0].getAttribute('type') == 'text') {
+                arrValues.push("'" + element.childNodes[0].value + "'");
+            }
+        }
+    }
+    
+    // The final output.
+    document.getElementById('output').innerHTML = arrValues;
+    //console.log (arrValues);  
+}
+ 
+    
+    
